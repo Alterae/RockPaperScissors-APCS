bbbd619c7cc780f3a69eb5ba19ba9e661ad31cf6
Michelle S <progamer.firestorm@gmail.com>
2021-08-20 14:34:59 -0600

    Initial commit

---
+++ src/RockPaperScissors.java
@@ -0,0 +1,14 @@
+public class RockPaperScissors {
+
+    // TODO: Fill with actual results.
+    // Use this to check who wins against whom.
+    private static final boolean[][] outcomes = {
+            {false, false, false},
+            {false, false, false},
+            {false, false, false},
+    };
+
+    public static void main(String[] args) {
+        // TODO: Implement program.
+    }
+}
